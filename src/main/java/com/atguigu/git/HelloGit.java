7de6e0941f672a418f99cdad0dc2fb29e5358fdf
Lazy <2545896165@qq.com>
2023-08-09 10:45:17 +0800

    第2次提交

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hellp");
+        System.out.println("how are you");
+        System.out.println("1");
     }
 }
